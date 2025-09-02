02130afeaa600ef14e79783f1a353da629c2f686
Arun18-apx <arun.s00077@gmail.com>
2025-09-02 15:17:35 +0530

    dsd

---
+++ revrese.py
@@ -0,0 +1,14 @@
+n="691"
+res=""
+count=0
+dic={"1":"1","0":"0","8":"8","6":"9","9":"6"}
+for i in range(len(n)-1,-1,-1):
+    if n[i] in dic:
+        res+=dic.get(n[i]) 
+    else:
+        count=1
+        break
+if count==0 and res==n:
+    print("True")
+else:    
+    print("False")
